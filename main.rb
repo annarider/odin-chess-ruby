dcad4da6e71640a02243fa844f542e8bb6b3fc0d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-19 12:53:52 -0600

    chore: set up project management for Chess

---
+++ main.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# main contains the main execution
+# for running a Chess game
+
+require_relative 'lib/game'
+
+loop do
+  game = Game.new
+  game.start
+  game.play
+  puts 'Play again? Enter y for yes (y): '
+  break unless gets.chomp.downcase == 'y'
+end
