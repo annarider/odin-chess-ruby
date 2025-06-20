485c20e5bdc0abb732f9a924bc39bc479c2e32bb
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-20 11:26:09 -0600

    feat: add config file to avoid hard-coded values

---
+++ lib/configuration.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Configuration stores game settings.
+module Configuration
+  BOARD_GRID_LENGTH = 8
+  DARK_SQUARE_COLOR = 'blue'
+  LIGHT_SQUARE_COLOR = 'white'
+end
