e086801304b6fa205870d30bb84317397912f139
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-24 09:52:00 -0600

    feat: add fore & background colors to text

--- spike/interface.rb
+++ spike/interface.rb
@@ -7,30 +7,61 @@ module ColoredString
   RGB_COLOR_MAP = {
     cyan: '139;233;253',
     green: '80;250;123',
-    red: '255;85;85'
+    red: '255;85;85',
+    cream: '240;217;181',
+    coffee: '181;136;99',
+    eggshell: '238;238;210',
+    sage: '118;150;86',
+    yellow: '255;206;84',
+    amber: '209;139;71',
+    gray: '232;235;239',
+    slate: '125;135;150'
   }
 
   refine String do
-    def fg_color(color_name)
+    def color(color_name, ground: fore)
       rgb_value = RGB_COLOR_MAP[color_name]
-      "\e[38;2;#{rgb_value}m#{self}\e[0m"
+      "\e[#{ground == 'fore' ? 38 : 48};2;#{rgb_value}m#{self}\e[0m"
     end
   end
 end
 
-class display
+class Display
+  using ColoredString
+
   def display_board(board)
-    puts 'New game: starting board'
-    puts ' a b c d e f g h'
+    puts 'New game: starting board'.color(:cyan, ground: 'fore')
+    puts ' a b c d e f g h'.color(:green, ground: 'back')
     board.grid.each do |rank|
       puts '\033[47m' if rank.nil?
     end
     green_rgb = "0;128;0"
     puts "\e[38;2;#{green_rgb}mhello\e[0m world"
   
-  
+    format_board_display = board.grid.map do |rank|
+      rank.map do |file|
+        if file.nil?
+          ' a '
+        else
+          ' ♟ '
+        end
+      end
+    end
+
+    format_board_display.each_with_index do |rank, rank_index|
+      # rank.fg_color(:slate) if index.even?
+      rank.each_with_index do |file, file_index|
+        combined_indices = rank_index + file_index
+        if combined_indices.even?
+          puts "#{file.color(:slate, ground: 'back')}"
+        else
+          puts "#{file.color(:gray, ground: 'back')}"
+        end
+      end 
+    end
   end  
 end
 
 board = Chess::Board.new
-display_board(board)
+display = Display.new
+display.display_board(board)
