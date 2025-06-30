78ae324c6f3ae737c035a543505c300eb986eaec
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-06-30 12:20:48 -0600

    feat: add Position class + test
    - capture a value object that represents a position
    (that is, a square) on the chess board
    - Position class has a few helper methods, such
    as taking a row and column and representing it in
    numeric coordinates following array-based indices
    - there's a comparison function
    - placeholder functions exist for future methods

--- lib/chess/position.rb
+++ lib/chess/position.rb
@@ -9,9 +9,47 @@
 # @example Create a new Position
 # position = Position.new(row, column)
 #
-class Position
+module Chess
+  class Position
+    attr :row, :column
+    
+    def self.from_numeric(row_number, column_number)
+      [row_number, column_number]
+    end
 
-  def initialize(rank: row_number, file: column_number)
+    def self.from_rank_file(string)
+      # TODO
+    end
+
+    def initialize(row_number, column_number)
+      @row = row_number
+      @column = column_number
+    end
+
+    def rank
+      # TODO
+    end
+
+    def file
+      # TODO
+    end
+
+    def rank_and_file
+      # TODO
+    end
+
+    def +(other)
+      # TODO
+    end
+
+    def ==(other)
+      row == other.row && column == other.column
+    end
+
+    def up(row_number, column_number)
+      # return a position one rank "up" from the perspective
+      # of this method's receiver
       
+    end
   end
 end
--- spec/chess/position_spec.rb
+++ spec/chess/position_spec.rb
@@ -1,8 +1,42 @@
 # frozen_string_literal: true
 
-require_relative '../lib/position'
+require_relative '../../lib/chess'
 
 # Tests for the Chess Position class
 
-describe Chess: Position do
+describe Chess::Position do
+  describe 'class methods' do
+    describe '.from_numeric' do
+      context 'when passing in the topmost leftmost square' do
+        it 'returns the position using array index coordinates' do
+          result = described_class.from_numeric(0, 0)
+          expect(result).to eq([0, 0])
+        end
+      end
+      context 'when passing in the bottom rightmost square' do
+        it 'returns the position using array index coordinates' do
+          result = described_class.from_numeric(7, 7)
+          expect(result).to eq([7, 7])
+        end
+      end
+    end
+  end
+  describe 'instance methods' do
+    subject(:test_position) { described_class }
+    describe '#==' do
+      it 'returns true when the positions are the same' do
+        top_left_position = described_class.new(0, 0)
+        test_position = described_class.new(0, 0)
+        result = top_left_position == test_position
+        expect(result).to be true
+      end
+
+      it 'returns false when the positions are not the same' do
+        top_left_position = described_class.new(0, 0)
+        test_position = described_class.new(7, 7)
+        result = top_left_position == test_position
+        expect(result).to be false
+      end
+    end
+  end
 end
