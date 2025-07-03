fbd95762077b30adbf3f76acd0247b1d7317db9f
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-03 11:46:18 -0600

    feat: add Pawn#move method + test
    - create hash to store direction/vector deltas
    - hash makes it easier to send command for a move
    direction
    - write #move method with helper methods
    - follow "tell, don't ask" approach
    - add tests

--- lib/chess/piece/pawn.rb
+++ lib/chess/piece/pawn.rb
@@ -16,31 +16,43 @@ module Chess
   class Pawn < Piece
     attr_accessor :position
 
+    PAWN_MOVE_SQUARES = {
+      forward_one: [1, 0],
+      forward_two: [2, 0],
+      diagonal_left: [1, 1],
+      diagonal_right: [1, -1]
+    }
+
     def initialize(position, color: :white, moved: false)
       @position = position
       validate_and_set_color(color)
       @moved = false
     end
 
-    def move
-      offset_delta = calculate_color_offset
-      new_coordinates = calculate_new_coordinates(offset_delta)
-      new_position = create_new_position(new_coordinates)
-      unless new_position.in_bound?
-        raise ArgumentError "Out-of-bounds: position #{new_position} is off game board"
-      end
-      
+    def move(direction)
+      adjusted_row_delta, adjusted_column_delta = unpack_move_deltas(direction)
+      new_coordinates = calculate_new_coordinates(adjusted_row_delta, adjusted_column_delta)
+      p new_coordinates
+      raise ArgumentError, "Out-of-bounds: position #{new_coordinates} is off game board" if new_coordinates.nil?
+
+      new_position = create_new_position(new_coordinates) if new_coordinates
       @position = new_position
     end
 
     private
 
-    def calculate_color_offset
-      color == :white ? -1 : 1
+    def unpack_move_deltas(direction)
+      PAWN_MOVE_SQUARES[direction].map do |delta|
+        adjust_direction_using_color(delta)
+      end
     end
 
-    def calculate_new_coordinates(offset_delta)
-      position.transform_coordinates(offset_delta, 0)
+    def adjust_direction_using_color(squares)
+      color == :white ? (-1 * squares) : squares
+    end
+
+    def calculate_new_coordinates(row_delta, column_delta)
+      position.transform_coordinates(row_delta, column_delta)
     end
 
     def create_new_position(coordinates)
--- spec/chess/piece/pawn_spec.rb
+++ spec/chess/piece/pawn_spec.rb
@@ -5,11 +5,19 @@ require_relative '../../../lib/chess'
 # Tests for the Chess Pawn class
 
 describe Chess::Pawn do
-  let(:white_pawn) { described_class.new(Chess::Position.new(6, 0)) }
+  let(:leftmost_white_pawn) { 
+    described_class.new(Chess::Position.new(6, 0))
+  }
+  let(:rightmost_black_pawn) { 
+    described_class.new(Chess::Position.new(1, 7), color: :black)
+  }
+  let(:middle_black_pawn) {
+    described_class.new(Chess::Position.new(1, 3), color: :black)
+  }
   describe '#moved?' do
     context 'when starting a new game and pawn has not moved' do
       it 'returns false thanks to inheriting from piece superclass' do
-        result = white_pawn.moved?
+        result = leftmost_white_pawn.moved?
         expect(result).to be false
       end
     end
@@ -18,39 +26,68 @@ describe Chess::Pawn do
   describe '#mark_as_moved!' do
     context 'after the pawn has moved' do
       it 'shows moved? status as true to reflect new move status' do
-        white_pawn.mark_as_moved!
-        expect(white_pawn).to be_moved
+        leftmost_white_pawn.mark_as_moved!
+        expect(leftmost_white_pawn).to be_moved
       end
     end
   end
 
   describe '#move' do
-    context 'when starting a new game and pawn moves 1 space up' do
-      it 'returns the new position after the leftmost white pawn moved' do
-        new_position = white_pawn.move
-        expect(new_position).to eq(Chess::Position.new(5, 0))
-        expect(white_pawn.position).to eq(new_position)
-      end
-      it 'returns the new position after the leftmost black pawn moved' do
-        black_pawn = described_class.new(Chess::Position.new(1, 0), color: :black)
-        new_position = black_pawn.move
-        expect(new_position).to eq(Chess::Position.new(2, 0))
-        expect(black_pawn.position).to eq(new_position)
+    context 'when the pawn is moving straight' do
+      context 'when starting a new game and pawn moves 1 space up' do
+        it 'returns the new position after the leftmost white pawn moved' do
+          new_position = leftmost_white_pawn.move(:forward_one)
+          expect(new_position).to eq(Chess::Position.new(5, 0))
+          expect(leftmost_white_pawn.position).to eq(new_position)
+        end
+        it 'returns the new position after the leftmost black pawn moved' do
+          leftmost_black_pawn = described_class.new(Chess::Position.new(1, 0), color: :black)
+          new_position = leftmost_black_pawn.move(:forward_one)
+          expect(new_position).to eq(Chess::Position.new(2, 0))
+          expect(leftmost_black_pawn.position).to eq(new_position)
+        end
+
+        it 'returns the new position after the rightmost black pawn' do
+          new_position = rightmost_black_pawn.move(:forward_one)
+          expect(new_position).to eq(Chess::Position.new(2, 7))
+          expect(rightmost_black_pawn.position).to eq(new_position)
+        end
       end
 
-      it 'returns the new position after the rightmost black pawn' do
-        black_pawn = described_class.new(Chess::Position.new(1, 7), color: :black)
-        new_position = black_pawn.move
-        expect(new_position).to eq(Chess::Position.new(2, 7))
-        expect(black_pawn.position).to eq(new_position)
+      context 'when starting a new game and pawn moves 2 squares up' do
+        it 'returns the new position after middle white pawn moved' do
+          new_position = leftmost_white_pawn.move(:forward_two)
+          expect(new_position).to eq(Chess::Position.new(4, 0))
+          expect(leftmost_white_pawn.position).to eq(new_position)
+        end
+
+        it 'returns the new position after middle black pawn moved' do
+          new_position = middle_black_pawn.move(:forward_two)
+          expect(new_position).to eq(Chess::Position.new(3, 3))
+          expect(middle_black_pawn.position).to eq(new_position)
+        end
       end
     end
-
-    context 'when starting a new game and pawn moves 2 squares up' do
-      it 'returns the new position after middle white pawn moved' do
-        new_position = white_pawn.move(2)
-        expect(new_position).to eq(Chess::Position.new(4, 0))
-        expect(white_pawn.move(2)).to eq(new_position)
+    context 'when the pawn is moving diagonally to capture' do
+      context 'when the white pawn moves 1 up diagonal left square' do
+        it 'returns an error when the leftmost white pawn tries to move' do
+          expect { leftmost_white_pawn.move(:diagonal_left) }.to raise_error(ArgumentError)
+        end
+        it 'returns the new position when the leftmost white moves diagonally right' do
+          new_position = leftmost_white_pawn.move(:diagonal_right)
+          expect(new_position).to eq(Chess::Position.new(5, 1))
+        end
+      end
+      context 'when the middle black pawn moves diagonally to capture' do
+        it 'returns the new position' do
+          new_position = middle_black_pawn.move(:diagonal_left)
+          expect(new_position).to eq(Chess::Position.new(2, 4))
+        end
+      end
+    end
+    context 'when the rightmost black pawn moves diagonally to capture' do
+      it 'returns an error when rightmost black pawn moves diagonally left' do
+        expect { rightmost_black_pawn.move(:diagonal_left) }.to raise_error(ArgumentError)
       end
     end
   end
