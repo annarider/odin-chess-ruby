9ba7d5c15939fb49cf8894727f7c93c98ebd2124
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-07 10:22:49 -0600

    feat: add custom Out Of Bounds Error
    - add a custom Out of Bounds Error class to issue
    message when a Position if out of bounds
    - running the test made me realize, I incorrectly
    used keyword arguments
    - even though tests were passing, I was accidentally
    passing the color, i.e. :white and :black to the
    position positional argument, instead of passing
    it to the keyword argument color:
    - fixed the tests by adding the keyword parameter
    color:

--- lib/chess/piece/piece.rb
+++ lib/chess/piece/piece.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 
 module Chess
+  # Custom Error class to handle when a
+  # Position object is out of bounds
+  # Cite: https://stackoverflow.com/a/5369746
+  class OutOfBoundsError < StandardError
+    attr_reader :position
+
+    def initialize(position, message = nil)
+      @position = position
+      message ||= "Position #{position} is off the board"
+      super(message)
+    end
+  end
+
   # Piece is a super class which
   # defines behavior for pieces in Chess.
   #
@@ -8,13 +21,13 @@ module Chess
   # functionality in the game. Subclasses,
   # such as rook and pawn, will inherit
   # methods from this Piece superclass.
-  #
   class Piece
-    attr_accessor :moved
+    attr_accessor :moved, :position
     attr_reader :color
 
-    def initialize(color, moved: false)
-      validate_and_set_color(color)
+    def initialize(position, color: :white, moved: false)
+      @position = position
+      @color = validate_and_set_color(color)
       @moved = moved
     end
 
@@ -35,5 +48,10 @@ module Chess
 
       @color = new_color
     end
+
+    def raise_out_of_bounds_error(position)
+      raise Chess::OutOfBoundsError.new(position)
+    end
+
   end
 end
--- spec/chess/piece/piece_spec.rb
+++ spec/chess/piece/piece_spec.rb
@@ -5,7 +5,8 @@ require_relative '../../../lib/chess'
 # Tests for the Chess Piece class
 
 describe Chess::Piece do
-  let(:new_piece) { described_class.new(:white) }
+  let(:white_position) { Chess::Position.new(6, 0) }
+  let(:new_piece) { described_class.new(white_position, color: :white) }
   describe '#color' do
     context 'when initializing to an allowed color' do
       it 'allows creating a new white piece' do
@@ -13,12 +14,13 @@ describe Chess::Piece do
         expect(new_piece.color).to eq(:white)
       end
       it 'allows creating a new black piece' do
-        black_piece = described_class.new(:black)
+        black_position = Chess::Position.new(1, 0)
+        black_piece = described_class.new(black_position, color: :black)
         expect(black_piece).to be_a(described_class)
         expect(black_piece.color).to eq(:black)
       end
       it 'fails to create a green piece' do
-        expect { described_class.new(:green) }.to raise_error(ArgumentError)
+        expect { described_class.new(white_position, color: :green) }.to raise_error(ArgumentError)
       end
     end
   end
