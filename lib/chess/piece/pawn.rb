f7dc6303028967c653ef3bd78cd9b65ed8be4b6b
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-10 14:50:44 -0600

    style: change to use FEN notation conventions
    - replace my made-up symbol names with FEN
    notation symbols

--- lib/chess/piece/pawn.rb
+++ lib/chess/piece/pawn.rb
@@ -23,7 +23,7 @@ module Chess
       end
     end
 
-    PAWN_MOVE_SQUARES = {
+    MOVE_DIRECTIONS = {
       forward_one: [1, 0],
       forward_two: [2, 0],
       diagonal_left: [1, 1],
@@ -34,12 +34,10 @@ module Chess
       super
     end
 
-    def move(direction)
-      block_forward_two(direction) if moved == true
-      coordinates = unpack_move_deltas(direction)
-      new_position = create_new_position(coordinates)
-      raise_out_of_bounds_error(new_position) if new_position.nil?
-      @position = new_position
+    protected
+
+    def legal_move?(direction)
+      block_forward_two(direction)
     end
 
     private
@@ -48,26 +46,6 @@ module Chess
       return unless direction == :forward_two && moved == true
 
       raise InvalidMove, 'Pawn has already moved and can only advance 1 square'
-    end
-
-    def unpack_move_deltas(direction)
-      unless PAWN_MOVE_SQUARES.include?(direction)
-        raise ArgumentError, "Invalid direction: #{direction}. Allowed are: #{PAWN_MOVE_SQUARES}"
-      end
-
-      PAWN_MOVE_SQUARES[direction].map do |delta|
-        adjust_direction_using_color(delta)
-      end
-    end
-
-    def adjust_direction_using_color(squares)
-      color == :white ? (-1 * squares) : squares
-    end
-
-    def create_new_position(coordinates)
-      row_delta = coordinates[0]
-      column_delta = coordinates[1]
-      position.transform_coordinates(Position.new(row_delta, column_delta))
     end
   end
 end
--- lib/chess/piece/piece.rb
+++ lib/chess/piece/piece.rb
@@ -39,6 +39,18 @@ module Chess
       @moved = true
     end
 
+    def move(direction)
+      coordinates = unpack_move_deltas(direction)
+      new_position = create_new_position(coordinates)
+      raise_out_of_bounds_error(new_position) if new_position.nil?
+      legal_move = legal_move?(direction)
+      @position = new_position if legal_move
+    end
+
+    protected
+
+    def legal_move?(direction); end
+
     private
 
     def validate_and_set_color(new_color)
@@ -47,6 +59,26 @@ module Chess
       end
 
       @color = new_color
+    end
+
+    def unpack_move_deltas(direction)
+      unless MOVE_DIRECTIONS.include?(direction)
+        raise ArgumentError, "Invalid direction: #{direction}. Allowed are: #{MOVE_DIRECTIONS}"
+      end
+
+      MOVE_DIRECTIONS[direction].map do |delta|
+        adjust_direction_using_color(delta)
+      end
+    end
+
+    def adjust_direction_using_color(squares)
+      color == :white ? (-1 * squares) : squares
+    end
+
+    def create_new_position(coordinates)
+      row_delta = coordinates[0]
+      column_delta = coordinates[1]
+      position.transform_coordinates(Position.new(row_delta, column_delta))
     end
 
     def raise_out_of_bounds_error(position)
