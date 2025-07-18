c24d85e8dd510424c1b104602737e82a7a7030b4
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-18 12:39:29 -0600

    feat: constructor injection DRY code with defaults
    - the `Board.initial_start` worked for naive
    constructor injection but was not scalable
    - with the need to support `Board.from_fen`, the
    constructor needs to get DRY-ed up so I don't have
    to repeat assignment in Board.from_fen
    - use idiomatic Ruby to set keyword arguments with
    defaults

--- lib/chess/board.rb
+++ lib/chess/board.rb
@@ -14,16 +14,22 @@ module Chess
   #
   class Board
     include Chess::FEN
+    extend Chess::FEN
     extend Chess::ChessNotation
     attr_accessor :grid, :active_color, :white_castle_kingside,
                   :white_castle_queenside, :black_castle_kingside,
                   :black_castle_queenside, :en_passant_square, :half_move_clock,
                   :full_move_number
 
-    def initialize(grid:, active_color:, white_castle_kingside:,
-                   white_castle_queenside:, black_castle_kingside:,
-                   black_castle_queenside:, en_passant_square:,
-                   half_move_clock:, full_move_number:)
+    def initialize(grid: default_grid, 
+      active_color: Chess::ChessNotation::WHITE_PLAYER, 
+      white_castle_kingside: ChessNotation::WHITE_CASTLE_KINGSIDE,
+      white_castle_queenside: ChessNotation::WHITE_CASTLE_QUEENSIDE,
+      black_castle_kingside: ChessNotation::BLACK_CASTLE_KINGSIDE,
+      black_castle_queenside: ChessNotation::BLACK_CASTLE_QUEENSIDE,
+      en_passant_square: nil,
+      half_move_clock: 0,
+      full_move_number: 1)
       @grid = grid
       @active_color = active_color
       @white_castle_kingside = white_castle_kingside
@@ -37,17 +43,20 @@ module Chess
 
     class << self
       def initial_start(add_pieces: true)
-        default_grid = Array.new(Chess::Config::GRID_LENGTH) { Array.new(Config::GRID_LENGTH) }
-        setup_pieces(default_grid) if add_pieces
-        new(grid: default_grid, active_color: Chess::ChessNotation::WHITE_PLAYER,
-            white_castle_kingside: ChessNotation::WHITE_CASTLE_KINGSIDE,
-            white_castle_queenside: ChessNotation::WHITE_CASTLE_QUEENSIDE,
-            black_castle_kingside: ChessNotation::BLACK_CASTLE_KINGSIDE,
-            black_castle_queenside: ChessNotation::BLACK_CASTLE_QUEENSIDE,
-            en_passant_square: nil, half_move_clock: 0, full_move_number: 1)
+        setup_starting_grid = setup_pieces(default_grid) if add_pieces
+        new(grid: setup_starting_grid)
+      end
+
+      def from_fen(fen_string)
+        parsed_data = parse_fen(fen_string)
+        new(**parsed_data)
       end
 
       private
+
+      def default_grid
+        Array.new(Chess::Config::GRID_LENGTH) { Array.new(Config::GRID_LENGTH) }
+      end
 
       def setup_pieces(grid)
         Chess::Piece::INITIAL_POSITIONS.each do |(rank, file), piece|
