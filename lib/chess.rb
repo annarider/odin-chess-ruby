b789ac2a8a9b73d652a23888bb7de4924cc9a348
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-21 11:40:27 -0600

    chore: update file imports

--- lib/chess.rb
+++ lib/chess.rb
@@ -8,9 +8,11 @@ require_relative 'chess/to_fen'
 require_relative 'chess/display'
 require_relative 'chess/directions_data'
 require_relative 'chess/piece_data'
+require_relative 'chess/move_calculator'
+require_relative 'chess/move_validator'
 require_relative 'chess/board'
 require_relative 'chess/position'
-require_relative 'chess/game_state'
+require_relative 'chess/move'
 
 # top-level namespace for organization
 # and contain the library files for
