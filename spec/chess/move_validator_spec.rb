261618aa10d16a348733bc63f9e611ff2d08f602
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-24 10:25:28 -0600

    test: add test for #valid_destination?

--- spec/chess/move_validator_spec.rb
+++ spec/chess/move_validator_spec.rb
@@ -28,17 +28,24 @@ describe Chess::Board do
       end
     end
 
-    context 'when a black knight captures a white pawn' do
-      it 'returns true' do
-        knight_start = Chess::Position.from_algebraic('f6')
-        knight_destination = Chess::Position.from_algebraic('e4')
-        move = Chess::Move.new(from_position: knight_start, to_position: knight_destination, piece: 'n')
+    context 'when a black knight captures' do
+      let(:knight_start) { Chess::Position.from_algebraic('f6') }
+      let(:knight_destination) { Chess::Position.from_algebraic('e4') }
+      let(:move) { Chess::Move.new(from_position: knight_start, to_position: knight_destination, piece: 'n') }
+      before do
         board.place_piece(knight_start, 'n')
+      end
+      it 'returns true for a white pawn' do
         board.place_piece(knight_destination, 'P')
         expect(board.valid_move?(move)).to be true
-
       end
       
+      it 'returns false for a black pawn' do
+        board.place_piece(knight_destination, 'p')
+        expect(board.valid_move?(move)).to be false
+      end
     end
+
+      
   end
 end
