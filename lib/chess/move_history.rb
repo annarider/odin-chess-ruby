03c645a9fa7f14a1580addf374d4bbaa08fbab41
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-25 11:25:45 -0600

    feat: add past_position counter for 3-fold rule

--- lib/chess/move_history.rb
+++ lib/chess/move_history.rb
@@ -1,14 +1,38 @@
 module Chess
   class MoveHistory
-    attr_accessor :move_history, :position_hash
+    include Chess::FromFEN
+    attr_accessor :move_history, :past_positions
 
-    def initialize(move_history = [], position_hash = {})
+    def initialize(move_history = [], past_positions = [])
       @move_history = move_history
-      @position_hash = position_hash
+      @past_positions = past_positions
     end
 
     def add_move(move)
       @move_history << move
+      @past_positions << fen_position(move.fen) if fen_position(move.fen)
+    end
+
+    def move_count
+      move_history.count
+    end
+
+    def threefold_repetition?
+      return true if count_past_positions.values.any? { |count| count >= 3 }
+      
+      false
+    end
+
+    private
+
+    def count_past_positions
+      count = {}
+      past_positions.each { |position| count[position] += 1 }
+      count     
+    end
+
+    def fen_position(fen_string)
+      parse_fen_for_positions(fen_string)
     end
   end
 end
--- spec/chess/move_history_spec.rb
+++ spec/chess/move_history_spec.rb
@@ -5,19 +5,18 @@ require_relative '../../lib/chess'
 # Tests for Move History class
 
 describe Chess::MoveHistory do
-  subject(:move_history) { described_class.new }
+  subject(:history_from_start) { described_class.new }
 
   describe '#add_move' do
     context 'when the first move completes' do
       let(:knight_start) { Chess::Position.from_algebraic('b1') }
       let(:knight_destination) { Chess::Position.from_algebraic('c3') }
       let(:move) { Chess::Move.new(from_position: knight_start, to_position: knight_destination, piece: 'N') }
-      it 'returns the first move' do
-        result = move_history.add_move(move)
-        expect(result).to eq([move])
+      it 'increases the move count by 1' do
+        expect{ history_from_start.add_move(move) }.to change{ history_from_start.move_count}.by(1)
       end
-      it 'returns the hash key of the first move' do
-        
+      it 'increase the position hash by 1' do
+        expect { history_from_start.add_move(move) }.to change{ history_from_start.move}
       end
     end
   end
