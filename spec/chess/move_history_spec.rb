6e750c5e1a52da312113d885eb5909319b8fc07d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-29 13:39:22 -0600

    test: add tests for threefold reptition

--- spec/chess/move_history_spec.rb
+++ spec/chess/move_history_spec.rb
@@ -46,7 +46,7 @@ describe Chess::MoveHistory do
     end
     context 'when Polgar vs. Kasparov, 2002 led to perpetual check draw' do
       it 'returns true for threefold repetition' do
-        threefold_game_fens = [
+        threefold_repetition_fens = [
           '2r3k1/5pp1/3p3p/3Pp3/1p2P1P1/1P2BP2/5P1P/4R1K1 w - - 0 30',
           '2r3k1/5pp1/3p3p/3Pp3/1p2P1P1/1P2BP2/4QP1P/4R1K1 b - - 1 30',
           '2r5/4kpp1/3p3p/3Pp3/1p2P1P1/1P2BP2/4QP1P/4R1K1 w - - 2 31',
@@ -60,7 +60,28 @@ describe Chess::MoveHistory do
           '2r5/4kpp1/3p3p/3Pp3/1p2P1P1/1P2BP2/4QP1P/4R1K1 w - - 10 35',
           '2r5/4kpp1/3p3p/3Pp2Q/1p2P1P1/1P2BP2/5P1P/4R1K1 b - - 11 35'
         ]
-        threefold_game_fens.each {}
+        threefold_repetition_fens.each { |fen| history_from_start.add_to_position(fen) }
+        expect(history_from_start.threefold_repetition?).to be true
+      end
+    end
+    context 'when knights position repeats leading to draw' do
+      it 'returns true for threefold repetition' do
+        fens_knight_repetition = [
+          'r1bq1rk1/pp2nppp/2n1p3/3p4/2PP4/2N1PN2/PP3PPP/R1BQKB1R w KQ - 0 10',
+          'r1bq1rk1/pp2nppp/2n1p3/3p4/2PP4/2N1PN2/PP2BPPP/R1BQK2R b KQ - 1 10',
+          'r1bq1rk1/pp2nppp/4p3/1n1p4/2PP4/2N1PN2/PP2BPPP/R1BQK2R w KQ - 2 11',
+          'r1bq1rk1/pp2nppp/4p3/1n1p4/2PP4/4PN2/PP1NBPPP/R1BQK2R b KQ - 3 11',
+          'r1bq1rk1/pp2nppp/2n1p3/3p4/2PP4/4PN2/PP1NBPPP/R1BQK2R w KQ - 4 12',
+          'r1bq1rk1/pp2nppp/2n1p3/3p4/2PP4/2N1PN2/PP2BPPP/R1BQK2R b KQ - 5 12',
+          'r1bq1rk1/pp2nppp/4p3/1n1p4/2PP4/2N1PN2/PP2BPPP/R1BQK2R w KQ - 6 13',
+          'r1bq1rk1/pp2nppp/4p3/1n1p4/2PP4/4PN2/PP1NBPPP/R1BQK2R b KQ - 7 13',
+          'r1bq1rk1/pp2nppp/2n1p3/3p4/2PP4/4PN2/PP1NBPPP/R1BQK2R w KQ - 8 14',
+          'r1bq1rk1/pp2nppp/2n1p3/3p4/2PP4/2N1PN2/PP2BPPP/R1BQK2R b KQ - 9 14',
+          'r1bq1rk1/pp2nppp/4p3/1n1p4/2PP4/2N1PN2/PP2BPPP/R1BQK2R w KQ - 10 15',
+          'r1bq1rk1/pp2nppp/4p3/1n1p4/2PP4/4PN2/PP1NBPPP/R1BQK2R b KQ - 11 15'
+        ]
+        fens_knight_repetition.each { |fen| history_from_start.add_to_position(fen) }
+        expect(history_from_start.threefold_repetition?).to be true
       end
     end
   end
