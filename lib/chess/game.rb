236f69f3c95e1fc6ff749c2dcf60a2c9291f71c8
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-07-31 11:05:57 -0600

    feat: add new Game class + test skeleton

---
+++ lib/chess/game.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require_relative '../chess'
+
+# Game defines a game object
+# which handles game logic. 
+# It orchestrates creating board
+# objects, switching player
+# turns, checking for game over
+# and win conditions, etc.
+#
+# @example Create a new Game
+# game = Game.new
+#
+class Game
+  attr_accessor :active_color, :board, :half_move_clock,
+                  :full_move_number
+
+  def initialize(active_color: ChessNotation::WHITE_PLAYER, 
+                  board: Board.initial_start,
+                   half_move_clock: 0,
+                   full_move_number: 1)
+                   @active_color = active_color
+  @board = board
+  @half_move_clock = half_move_clock
+@full_move_number = full_move_number
+  end
+
+  def start
+  end
+
+  def current_player
+  end
+
+  def play
+  end
+
+  def play_turn
+   
+  end
+
+  def pick_column
+ 
+  end
+
+
+
+  def end_game?(column)
+  
+  end
+
+  def switch_turns
+  
+  end
+
+  private
+
+  def create_players(players_data)
+ 
+  end
+
+  def handle_game_end(column, name)
+    if board.winner?(column)
+      announce_winner(name)
+    else
+      announce_end
+    end
+  end
+
+  def announce_winner(name)
+    puts "Game over! #{name} won."
+  end
+
+  def announce_end
+    puts 'Game over! Nobody won.'
+  end
+end
---
+++ spec/chess/game_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/chess'
+
+# Tests for the Chess Game class
+
+describe Game do
+  let(:new_game) { Game.new }
+  let(:running_game) do
+  end
+
+  describe '#start' do
+    context 'when starting the game' do
+    end
+  end
+  describe '#current_player' do
+    context 'when the game starts' do
+    end
+  end
+  describe '#switch_turns' do
+    context 'when the current player ends a turn' do
+    end
+  end
+
+  describe '#play_turn' do
+    context 'when the current player gets a turn' do
+    end
+  end
+end
