3e63473d58285831acd4c0657d4c63b910d48441
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-08-07 16:56:48 -0600

    test: add test for PawnMoveValidator + attr_reader
    - add most basic test for PawnMoveValidator
    - check one square advance is a valid move
    - need access to instance variables so add
    attr_reader

--- lib/chess/services/pawn_move_validator.rb
+++ lib/chess/services/pawn_move_validator.rb
@@ -10,22 +10,44 @@ module Chess
   # - Two-square moves only for unmoved pawns
   # - Forward moves only to empty squares
   class PawnMoveValidator
+    attr_reader :start_position, :end_position
     def self.valid_move?(...)
       new(...).valid_move?
     end
 
     def initialize(move, move_history = [])
-      @from_position = move.from_position
-      @to_position = move.to_position
+      @start_position = move.from_position
+      @end_position = move.to_position
+      @piece = move.piece
+      @captured_piece = move.captured_piece
       @move_history = move_history
     end
 
     def valid_move?
-      if from_position.diagonal_move?(to_position)
-        diagonal_move_valid?
+      if start_position.diagonal_move?(end_position)
+        capture_move_valid? # pawn can't move to an empty square
       else
         forward_move_valid?
       end
+    end
+
+    private
+
+    def capture_move_valid?
+      return false unless captured_piece
+
+      PieceHelpers.enemy_color?
+    end
+
+    def forward_move_valid?
+      # pawn hasn't moved yet, allowed to move 2 squares
+      return false if start_position.two_rank_move?(end_position) && pawn_has_moved?
+
+      true
+    end
+
+    def pawn_has_moved?
+      move_history.has_moved?(start_position)
     end
   end
 end
--- spec/chess/services/pawn_move_validator_spec.rb
+++ spec/chess/services/pawn_move_validator_spec.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
+require_relative '../../../lib/chess'
+
 # Tests for Pawn Move Validator
 
-describe ''
+describe Chess::PawnMoveValidator do
+  describe '.valid_move?' do
+    let(:move) { instance_double(Chess::Move) }
+    let(:move_history) { instance_double(Chess::MoveHistory) }
+    let(:start_position) { double('start_position') }
+    let(:end_position) { double('end_position') }
+    
+    before do
+      allow(move).to receive(:from_position).and_return(start_position)
+      allow(move).to receive(:to_position).and_return(end_position)
+      allow(move).to receive(:piece).and_return('P')
+      allow(move).to receive(:captured_piece).and_return(nil)
+    end
+    context 'when white pawn advances 1 square' do
+      before do
+        allow(start_position).to receive(:diagonal_move?).with(end_position).and_return(false)
+        allow(start_position).to receive(:two_rank_move?).with(end_position).and_return(false)
+      end
+        it 'returns true' do
+          result = described_class.valid_move?(move, move_history)
+          expect(result).to be true
+        end
+    end
+  end
+end
