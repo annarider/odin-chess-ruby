62e8727f9ae174a857e2eef7de42a9c3e4dd1cd6
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-08-12 10:35:39 -0600

    test: add outgoing commands test for MoveValidator

--- spec/chess/services/move_validator_spec.rb
+++ spec/chess/services/move_validator_spec.rb
@@ -74,21 +74,72 @@ describe Chess::MoveValidator do
       end
     end
     context 'when validating a pawn move' do
-      let(:pawn_start) { Chess::Position.from_algebraic('e2') }
-      let(:pawn_destination) { Chess::Position.from_algebraic('e4') }
-      let(:pawn_move) { Chess::Move.new(from_position: pawn_start, to_position: pawn_destination, piece: 'P') }
+      let(:white_pawn_start) { Chess::Position.from_algebraic('e2') }
+      let(:white_pawn_destination) { Chess::Position.from_algebraic('e4') }
+      let(:white_pawn_move) { Chess::Move.new(from_position: white_pawn_start, to_position: white_pawn_destination, piece: 'P') }
       let(:move_history) { [] }
       before do 
-        board.place_piece(pawn_start, 'P')
+        board.place_piece(white_pawn_start, 'P')
         allow(Chess::MoveCalculator).to receive(:generate_possible_moves)
-          .and_return([pawn_destination])
+          .and_return([white_pawn_destination])
         allow(Chess::PawnMoveValidator).to receive(:valid_move?).and_return(true)
       end
       it 'sends valid_move? message to PawnMoveValidator service' do
         expect(Chess::PawnMoveValidator).to receive(:valid_move?)
-          .with(pawn_move, move_history)
+          .with(white_pawn_move, move_history)
 
-          described_class.is_move_legal?(board, pawn_move, move_history)
+          described_class.is_move_legal?(board, white_pawn_move, move_history)
+      end
+    end
+    context 'when validating a black pawn move' do
+      let(:black_pawn_start) { Chess::Position.from_algebraic('d7') }
+      let(:black_pawn_destination) { Chess::Position.from_algebraic('d6') }
+      let(:black_pawn_move) { Chess::Move.new(from_position: black_pawn_start, to_position: black_pawn_destination, piece: 'p') }
+      let(:move_history) { [] }
+      before do
+        board.place_piece(black_pawn_start, 'p')
+        # Stub the other validation methods to isolate the outgoing command test
+        allow(Chess::MoveCalculator).to receive(:generate_possible_moves)
+          .and_return([black_pawn_destination])
+        allow(Chess::PawnMoveValidator).to receive(:valid_move?).and_return(true)
+      end
+      it 'sends valid_move? message to PawnMoveValidator with correct arguments' do
+        expect(Chess::PawnMoveValidator).to receive(:valid_move?)
+          .with(black_pawn_move, move_history)
+
+          described_class.is_move_legal?(board, black_pawn_move, move_history)
+      end
+    end
+    context 'when validating a non-pawn move' do
+      let(:knight_start) { Chess::Position.from_algebraic('b1') }
+      let(:knight_destination) { Chess::Position.from_algebraic('c3') }
+      let(:knight_move) { Chess::Move.new(from_position: knight_start, to_position: knight_destination, piece: 'N') }
+      let(:move_history) { [] }
+      before do
+        board.place_piece(knight_start, 'N')
+        allow(Chess::MoveCalculator).to receive(:generate_possible_moves)
+          .and_return([knight_destination])
+      end
+      it 'does not send valid_move? message to PawnMoveValidator' do
+        expect(Chess::PawnMoveValidator).not_to receive(:valid_move?)
+
+        described_class.is_move_legal?(board, knight_move, move_history)
+      end
+    end
+    context 'when PawnMoveValidator returns false' do
+      let(:invalid_pawn_start) { Chess::Position.from_algebraic('e3') }
+      let(:invalid_pawn_destination) { Chess::Position.from_algebraic('e5') }
+      let(:invalid_pawn_move) { Chess::Move.new(from_position: invalid_pawn_start, to_position: invalid_pawn_destination, piece: 'P') }
+      let(:move_history) { [] }
+      before do
+        board.place_piece(invalid_pawn_start, 'P')
+        allow(Chess::MoveCalculator).to receive(:generate_possible_moves)
+          .and_return([invalid_pawn_destination])
+        allow(Chess::PawnMoveValidator).to receive(:valid_move?).and_return(false)
+      end
+      it 'returns false when pawn validation fails' do
+        result = described_class.is_move_legal?(board, invalid_pawn_move, move_history)
+        expect(result).to be false
       end
     end
   end
