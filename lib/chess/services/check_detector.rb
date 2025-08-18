e8e65193cf3d813d2cec6177da7d5ade3048fe8d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-08-18 11:33:43 -0600

    feat: add logic for CheckDetector.in_check?
    - fix logic for getting all the opponent's moves
    - call MoveCalculator to get all the moves
    - use flat_map to flatten 2D array that gets
    returned
    - check if the king's position is in any of the
    possible opponent's move positions

--- lib/chess/services/check_detector.rb
+++ lib/chess/services/check_detector.rb
@@ -15,20 +15,26 @@ module Chess
 
     def in_check?
       king_position = board.find_king(active_color)
+      return false unless king_position
+
       opponent_color = PieceHelpers.opponent_color(active_color)
       # retrieve data on all of opponent's pieces & their positions
-      opponent_piece_data = board.find_all_pieces(opponent_color)
+      opponent_pieces = board.find_all_pieces(opponent_color)
       # calculate all moves possible from opponent pieces
-      p opponent_moves = find_all_pieces_moves(opponent_piece_data)
+      opponent_moves = find_all_pieces_moves(opponent_pieces)
+      find_king_in_opponent_moves?(king_position, opponent_moves)
     end
 
     private
 
     def find_all_pieces_moves(piece_data)
-      possible_move_positions = []
-      piece_data.each do |position, piece|
-        position[:position]
+      piece_data.flat_map do |position, piece|
+        MoveCalculator.generate_possible_moves(position, piece)
       end
+    end
+
+    def find_king_in_opponent_moves?(king_position, opponent_moves)
+      opponent_moves.any? { |position| position == king_position }
     end
   end
 end
