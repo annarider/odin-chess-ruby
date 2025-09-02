3cdc43ac6a32a2d82fa3b8811be7825839545c2d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-09-02 10:00:41 -0600

    feat: add MoveCommander class to execute moves
    - add new class MoveCommander which is responsible
    for executing moves
    - MoveCommander serves as an agent for Game to tell
    Board to update its state
    - add to file load path

---
+++ lib/chess/services/move_commander.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Chess
+  # MoveCommander is responsible for executing moves
+  # and coordinating all board state updates.
+  # It orchestrates the sequence of operations
+  # needed when a move is played, telling the
+  # board what updates to perform.
+  class MoveCommander
+    attr_reader :board, :move, :piece, :from_position, :to_position
+
+    def self.execute_move(...)
+      new(...).execute_move
+    end
+
+    def initialize(board, move)
+      @board = board
+      @move = move
+      @piece = move.piece
+      @from_position = move.from_position
+      @to_position = move.to_position
+    end
+
+    def execute_move
+      update_piece_positions
+      update_castling_rights if affects_castling?
+      update_en_passant_target if pawn_double_move?
+    end
+
+    private
+
+    def update_piece_positions
+      board.update_position(from_position, to_position)
+    end
+
+    def update_castling_rights
+      board.update_castling_rights(move)
+    end
+
+    def update_en_passant_target
+      board.update_en_passant_target(move)
+    end
+
+    def affects_castling?
+      Piece::KING_PIECES.include?(piece) || Piece::ROOK_PIECES.include?(piece)
+    end
+
+    def pawn_double_move?
+      return false unless Piece::PAWN_PIECES.include?(piece)
+
+      row_diff = (to_position.row - from_position.row).abs
+      row_diff == 2
+    end
+  end
+end
