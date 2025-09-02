710711268df4c1f3c27d1f5f88d39a6343efdaad
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-09-02 14:27:47 -0600

    test: update Interface test for chess methods

--- spec/chess/helpers/interface_spec.rb
+++ spec/chess/helpers/interface_spec.rb
@@ -2,45 +2,370 @@
 
 require_relative '../../../lib/chess'
 
-# Tests for the Connect Four Chess::Interface module
-
 describe Chess::Interface do
-  describe '.request_players_data' do
-    context 'when game greets the players' do
-      it "requests and return the players' info" do
-        # allow(described_class).to receive(:gets).and_return('Anna', 'p', 'Alex', 'y')
-        # expect(described_class.request_players_data).to eq({ 'Anna' => 'p', 'Alex' => 'y' })
-      end
+  describe '.welcome' do
+    it 'displays welcome message' do
+      expected_output = <<~WELCOME
+        ♔♟️ Welcome to Chess. You will play a Chess game in the console.
+        We will have 2 players represented by color, white and black.
+        As with traditional chess games, white moves first.
+      WELCOME
+      
+      expect { described_class.welcome }.to output(expected_output).to_stdout
     end
   end
 
-  describe '.request_column' do
-    context 'when the player chooses a move' do
-      let(:valid_move) { '3' }
-
-      before do
-        allow(described_class).to receive(:gets).and_return(valid_move)
+  describe '.request_move' do
+    context 'when user enters quit commands' do
+      it 'returns quit action for "quit"' do
+        allow(described_class).to receive(:gets).and_return('quit')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:quit)
       end
-
-      it 'returns the column number' do
-        # expect(described_class.request_column).to eq(3)
+      
+      it 'returns quit action for "q"' do
+        allow(described_class).to receive(:gets).and_return('q')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:quit)
       end
+      
+      it 'returns quit action for "QUIT" (case insensitive)' do
+        allow(described_class).to receive(:gets).and_return('QUIT')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:quit)
+      end
+    end
+    
+    context 'when user enters save commands' do
+      it 'returns save action for "save"' do
+        allow(described_class).to receive(:gets).and_return('save')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:save)
+      end
+      
+      it 'returns save action for "s"' do
+        allow(described_class).to receive(:gets).and_return('s')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:save)
+      end
+    end
+    
+    context 'when user enters load commands' do
+      it 'returns load action for "load"' do
+        allow(described_class).to receive(:gets).and_return('load')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:load)
+      end
+      
+      it 'returns load action for "l"' do
+        allow(described_class).to receive(:gets).and_return('l')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:load)
+      end
+    end
+    
+    context 'when user enters valid chess moves' do
+      it 'returns move action with positions for space-separated move' do
+        allow(described_class).to receive(:gets).and_return('e2 e4')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:move)
+        expect(result[:from]).to be_a(Chess::Position)
+        expect(result[:to]).to be_a(Chess::Position)
+        expect(result[:from].to_algebraic).to eq('e2')
+        expect(result[:to].to_algebraic).to eq('e4')
+        expect(result[:raw_input]).to eq('e2 e4')
+      end
+      
+      it 'returns move action with positions for dash-separated move' do
+        allow(described_class).to receive(:gets).and_return('e2-e4')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:move)
+        expect(result[:from].to_algebraic).to eq('e2')
+        expect(result[:to].to_algebraic).to eq('e4')
+        expect(result[:raw_input]).to eq('e2-e4')
+      end
+      
+      it 'handles moves with extra whitespace' do
+        allow(described_class).to receive(:gets).and_return('  e2   e4  ')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:move)
+        expect(result[:from].to_algebraic).to eq('e2')
+        expect(result[:to].to_algebraic).to eq('e4')
+      end
+      
+      it 'handles uppercase moves' do
+        allow(described_class).to receive(:gets).and_return('E2 E4')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:move)
+        expect(result[:from].to_algebraic).to eq('e2')
+        expect(result[:to].to_algebraic).to eq('e4')
+      end
+    end
+    
+    context 'when user enters invalid input' do
+      it 'returns invalid action for incomplete move' do
+        allow(described_class).to receive(:gets).and_return('e2')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
+      end
+      
+      it 'returns invalid action for too many parts' do
+        allow(described_class).to receive(:gets).and_return('e2 e4 e5')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
+      end
+      
+      it 'returns invalid action for invalid square notation' do
+        allow(described_class).to receive(:gets).and_return('z9 a1')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
+      end
+      
+      it 'returns invalid action for non-chess input' do
+        allow(described_class).to receive(:gets).and_return('hello world')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
+      end
+    end
+    
+    it 'prompts user for input' do
+      allow(described_class).to receive(:gets).and_return('quit')
+      expected_prompt = "Enter your move (e.g., 'e2 e4' or 'e2-e4'), or type 'quit', 'save', or 'load':"
+      
+      expect { described_class.request_move }.to output(/#{Regexp.escape(expected_prompt)}/).to_stdout
+    end
+  end
 
-      context 'when the chosen column is invalid twice then valid' do
-        let(:letter) { 'd' }
-        let(:symbol) { '$' }
-        let(:valid_input) { '6' }
+  describe '.announce_turn' do
+    it 'announces white player turn' do
+      expected_output = "White's turn.\n"
+      
+      expect { described_class.announce_turn(Chess::ChessNotation::WHITE_PLAYER) }.to output(expected_output).to_stdout
+    end
+    
+    it 'announces black player turn' do
+      expected_output = "Black's turn.\n"
+      
+      expect { described_class.announce_turn(Chess::ChessNotation::BLACK_PLAYER) }.to output(expected_output).to_stdout
+    end
+  end
 
-        before do
-          allow(described_class).to receive(:gets).and_return(letter, symbol, valid_input)
-          allow(described_class).to receive(:puts)
-        end
+  describe '.announce_check' do
+    it 'announces white is in check' do
+      expected_output = "White is in check!\n"
+      
+      expect { described_class.announce_check(Chess::ChessNotation::WHITE_PLAYER) }.to output(expected_output).to_stdout
+    end
+    
+    it 'announces black is in check' do
+      expected_output = "Black is in check!\n"
+      
+      expect { described_class.announce_check(Chess::ChessNotation::BLACK_PLAYER) }.to output(expected_output).to_stdout
+    end
+  end
 
-        it 'completes loop after two incorrect tries' do
-          # described_class.request_column
-          # expect(described_class).to have_received(:puts).with(/Invalid/).twice
-          # expect(described_class.request_column).to eq(6)
-        end
+  describe '.announce_invalid_move' do
+    it 'displays invalid move message' do
+      expected_output = "❌ Invalid move. Please try again.\n"
+      
+      expect { described_class.announce_invalid_move }.to output(expected_output).to_stdout
+    end
+  end
+
+  describe '.request_save_filename' do
+    context 'when user provides a filename' do
+      it 'returns the provided filename' do
+        allow(described_class).to receive(:gets).and_return('my_game')
+        
+        result = described_class.request_save_filename
+        
+        expect(result).to eq('my_game')
+      end
+      
+      it 'strips whitespace from filename' do
+        allow(described_class).to receive(:gets).and_return('  my_game  ')
+        
+        result = described_class.request_save_filename
+        
+        expect(result).to eq('my_game')
+      end
+    end
+    
+    context 'when user provides empty filename' do
+      it 'returns timestamp-based filename' do
+        allow(described_class).to receive(:gets).and_return('')
+        allow(Time).to receive(:now).and_return(Time.new(2023, 12, 25, 14, 30, 45))
+        
+        result = described_class.request_save_filename
+        
+        expect(result).to eq('chess_game_20231225_143045')
+      end
+    end
+    
+    it 'prompts user for filename' do
+      allow(described_class).to receive(:gets).and_return('test')
+      expected_prompt = "Enter filename to save (without extension):\n"
+      
+      expect { described_class.request_save_filename }.to output(expected_prompt).to_stdout
+    end
+  end
+
+  describe '.request_load_filename' do
+    it 'returns the provided filename' do
+      allow(described_class).to receive(:gets).and_return('saved_game')
+      
+      result = described_class.request_load_filename
+      
+      expect(result).to eq('saved_game')
+    end
+    
+    it 'strips whitespace from filename' do
+      allow(described_class).to receive(:gets).and_return('  saved_game  ')
+      
+      result = described_class.request_load_filename
+      
+      expect(result).to eq('saved_game')
+    end
+    
+    it 'prompts user for filename' do
+      allow(described_class).to receive(:gets).and_return('test')
+      expected_prompt = "Enter filename to load (without extension):\n"
+      
+      expect { described_class.request_load_filename }.to output(expected_prompt).to_stdout
+    end
+  end
+
+  describe '.confirm_quit' do
+    context 'when user confirms quit' do
+      it 'returns true for "y"' do
+        allow(described_class).to receive(:gets).and_return('y')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be true
+      end
+      
+      it 'returns true for "yes"' do
+        allow(described_class).to receive(:gets).and_return('yes')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be true
+      end
+      
+      it 'returns true for "Y" (case insensitive)' do
+        allow(described_class).to receive(:gets).and_return('Y')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be true
+      end
+      
+      it 'returns true for "YES" (case insensitive)' do
+        allow(described_class).to receive(:gets).and_return('YES')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be true
+      end
+    end
+    
+    context 'when user does not confirm quit' do
+      it 'returns false for "n"' do
+        allow(described_class).to receive(:gets).and_return('n')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be false
+      end
+      
+      it 'returns false for "no"' do
+        allow(described_class).to receive(:gets).and_return('no')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be false
+      end
+      
+      it 'returns false for any other input' do
+        allow(described_class).to receive(:gets).and_return('maybe')
+        
+        result = described_class.confirm_quit
+        
+        expect(result).to be false
+      end
+    end
+    
+    it 'prompts user for confirmation' do
+      allow(described_class).to receive(:gets).and_return('y')
+      expected_prompt = "Are you sure you want to quit? (y/n)\n"
+      
+      expect { described_class.confirm_quit }.to output(expected_prompt).to_stdout
+    end
+  end
+
+  describe 'input validation' do
+    context 'when validating square notation' do
+      it 'accepts valid squares from a1 to h8' do
+        allow(described_class).to receive(:gets).and_return('a1 h8')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:move)
+      end
+      
+      it 'rejects squares with invalid files' do
+        allow(described_class).to receive(:gets).and_return('i1 a1')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
+      end
+      
+      it 'rejects squares with invalid ranks' do
+        allow(described_class).to receive(:gets).and_return('a9 a1')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
+      end
+      
+      it 'rejects squares with wrong length' do
+        allow(described_class).to receive(:gets).and_return('a11 b2')
+        
+        result = described_class.request_move
+        
+        expect(result[:action]).to eq(:invalid)
       end
     end
   end
